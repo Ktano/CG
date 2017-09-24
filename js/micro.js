cb6af4714ed80b96ca8a37672ada03065b74b96e
Caetano <pedrocaetano56@gmail.com>
2017-09-24 22:00:23 +0100

    includes table

--- js/micro.js
+++ js/micro.js
@@ -1,5 +1,4 @@
 var camera, scene, renderer;
-
 function render() {
     'use strict'
     renderer.render(scene, camera);
@@ -10,17 +9,19 @@ function createScene() {
     
     scene= new THREE.Scene();
     scene.add(new THREE.AxisHelper(10));
+
+    createCourse(0,0,0);
 }
 
 function createCamera(){
     'use strict';
 
-    camera= new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight,1,1000)
+    camera= new THREE.OrthographicCamera(-100, 100, 100, -100, 1, 1000 );
 
-    camera.position.x = 0;
-    camera.position.y = 0;
-    camera.position.z = 100;
-    camera.lookAt(scene.position)
+   camera.position.x = 0;
+   camera.position.y = 100;
+   camera.position.z = 0;
+   camera.lookAt(scene.position)
 
 }
 
@@ -37,4 +38,31 @@ function init() {
     createCamera();
     
     render();
+}
+
+function createCourse(x,y,z){
+    'use strict';
+
+    var table = new THREE.Object3D();
+
+    var material = new THREE.MeshBasicMaterial({color: 0xF0EAD6, wireframe: false});
+
+    addTable(table,0,0,0, material);
+
+    scene.add(table);
+
+    table.position.x=x;
+    table.position.y=y;
+    table.position.z=z;
+}
+
+function addTable(obj, x, y, z, material){
+    'use strict';
+
+    var geometry = new THREE.CubeGeometry(200,2,200);
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x,y,z);
+
+    obj.add(mesh);
+
 }
