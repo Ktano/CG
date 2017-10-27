f30c8c8a80e537ee3e146ba6b525a852d2c8fb19
Caetano <pedrocaetano56@gmail.com>
2017-10-27 19:10:51 +0100

    fim segunda entrega

--- js/micro.js
+++ js/micro.js
@@ -8,17 +8,18 @@ var updateCamera = false;
 var resized = false;
 var clock = new THREE.Clock(true);
 var orangeClock = new THREE.Clock(true);
-var currentMultiplier=1;
-var oranges=[];
+var currentMultiplier = 1;
+var oranges = [];
 
 /* Constants*/
 HEIGHT = 900;
 WIDTH = 1500;
 ACC = 0.05;
 MAX_SPEED = 12;
-DEC = 0.2;
-ORANGE_SPEED_MULTIPLIER=1.2;
-MAX_MULTIPLIER=3;
+DEC = 0.1;
+ORANGE_SPEED_MULTIPLIER = 1.2;
+MAX_MULTIPLIER = 3;
+NUMBER_ORANGES = 4
 
 function render() {
     "use strict";
@@ -34,11 +35,11 @@ function createScene() {
     scene.add(new THREE.AxisHelper(10));
 
     createCourse(0, 0, 0);
-/*
-    createOrangeObstacle(50, 95);
-    createOrangeObstacle(-375, 45);
-    createOrangeObstacle(700, -325);
-*/
+    /*
+        createOrangeObstacle(50, 95);
+        createOrangeObstacle(-375, 45);
+        createOrangeObstacle(700, -325);
+    */
     createButterObstacle(-375, -150);
     createButterObstacle(25, 30);
     createButterObstacle(350, 55);
@@ -94,22 +95,26 @@ function init() {
     window.addEventListener("keyup", onKeyUp);
 }
 
-function createOrangeObstacle(x, z, dir,sp) {
+function createOrangeObstacle(x, z, dir, sp) {
     "use strict";
 
-    var obstacle = new THREE.Object3D();
+    var obstacle = new colidable();
     obstacle.userData = {
+        type: "orange",
         moving: false,
         gameObject: true,
         direction: dir,
         speed: sp
     };
+
+
     addOrange(obstacle, 0, 0, 0);
     addCaule(obstacle, 0, 22.5, 0)
     scene.add(obstacle);
     obstacle.position.x = x;
     obstacle.position.y = 21;
     obstacle.position.z = z;
+    obstacle.updateboundings();
     return obstacle;
 }
 
@@ -128,8 +133,9 @@ function addCaule(obj, x, y, z) {
 function createButterObstacle(x, z) {
     "use strict";
 
-    var obstacle = new THREE.Object3D();
+    var obstacle = new colidable();
     obstacle.userData = {
+        type: "butter",
         moving: false,
         gameObject: true
     };
@@ -139,20 +145,23 @@ function createButterObstacle(x, z) {
     obstacle.position.x = x;
     obstacle.position.y = 21;
     obstacle.position.z = z;
+    obstacle.updateboundings();
 }
 
 function createCar(x, y, z) {
     "use strict";
 
-    car = new THREE.Object3D();
+    car = new colidable();
 
     car.userData = {
+        type: "car",
         moving: false,
         direction: new THREE.Vector3(-1, 0, 0),
         speed: 0,
         turning: 0,
         accelarating: 0,
-        gameObject: true
+        gameObject: true,
+        startposition: new THREE.Vector3(400, 3, 350)
     };
     addWing(car, -12.5, 1.5, 0);
     addWing(car, +13, 7.5, 0);
@@ -167,9 +176,9 @@ function createCar(x, y, z) {
     addFrontWheelConnections(car, -8.75, 1.5, -3);
     addCamera(car, 50, 25);
     scene.add(car);
-    car.position.x = x;
-    car.position.y = y;
-    car.position.z = z;
+    car.position.copy(car.userData.startposition);
+
+    car.updateboundings();
 }
 
 
@@ -315,17 +324,24 @@ function addTable(obj, x, y, z, material) {
 }
 
 function addCheerio(obj, x, y, z) {
-
-    var geometry = new THREE.TorusGeometry(5, 2, 16, 100)
+    var cheerio = new colidable();
+    var geometry = new THREE.TorusGeometry(5, 2, 16, 100);
     var material = new THREE.MeshBasicMaterial({
         color: randomColor(),
         wireframe: false
     });
+    cheerio.userData = {
+        type: "cheerio",
+        direction: new THREE.Vector3(0, 0, 0),
+        speed: 0,
+    }
     var mesh = new THREE.Mesh(geometry, material);
     mesh.position.set(x, y, z);
 
     mesh.rotation.x = Math.PI / 2;
-    obj.add(mesh);
+    cheerio.add(mesh);
+    cheerio.updateboundings();
+    obj.add(cheerio);
 }
 
 function addButter(obj, x, y, z) {
@@ -354,7 +370,25 @@ function addOrange(obj, x, y, z) {
     obj.add(mesh);
 }
 
+function intercept(current, other) {
+    return interceptSphere(current, other) && interceptBox(current, other)
+}
 
+function interceptSphere(current, other) {
+    var dist = current.boundingSphere.center.distanceTo(other.boundingSphere.center);
+    return dist < current.boundingSphere.radius + other.boundingSphere.radius;
+}
+
+function interceptBox(current, other) {
+    var box1 = current.boundingBox;
+    var box2 = current.boundingBox;
+    return box1.max.x > box2.min.x &&
+        box1.min.x < box2.max.x &&
+        box1.max.z > box2.min.z &&
+        box1.min.z < box2.max.z &&
+        box1.max.y > box2.min.y &&
+        box1.min.y < box2.max.y;
+}
 
 function randomColor() {
     colors = [0xee75be, 0x6e1fd4, 0x980606, 0xd09422, 0x97c14d, 0x6897bb]
@@ -436,8 +470,8 @@ function onKeyUp(e) {
 function animate() {
     "use strict";
 
-    /* Car rotation animation currently rotates with the car stopped*/
-    if (car.userData.turning != 0) {
+    /* Car rotation animation*/
+    if (car.userData.turning != 0 && car.userData.speed != 0) {
         var axis = new THREE.Vector3(0, 1, 0);
         var angle = 0.05 * car.userData.turning;
         car.userData.direction.applyAxisAngle(axis, angle);
@@ -475,21 +509,43 @@ function animate() {
         d.copy(car.userData.direction);
         car.position.add(d.multiplyScalar(car.userData.speed));
     }
+    car.updateboundings();
+
+    /*update cheerioes*/
+    scene.traverse(function (node) {
+        if (node.userData.type=="cheerio" && node.userData.speed!=0) {
+
+            if (node.userData.speed < 0) {
+                node.userData.speed += DEC;
+                if (node.userData.speed > 0)
+                node.userData.speed = 0;
+            }
+            if (node.userData.speed > 0) {
+                node.userData.speed -= DEC;
+                if (node.userData.speed < 0)
+                node.userData.speed = 0;
+            }
+            var d = new THREE.Vector3;
+            d.copy(node.userData.direction);
+            node.position.add(d.multiplyScalar(node.userData.speed));
+            node.updateboundings();
+        }
+    });
 
     /* Change between cameras*/
     if (updateCamera) {
         switch (currentCamera) {
             case 0:
                 camera = camera0;
-                resized=true;
+                resized = true;
                 break;
             case 1:
                 camera = camera1;
-                resized=true;
+                resized = true;
                 break;
             case 2:
                 camera = camera2;
-                resized=true;
+                resized = true;
                 break;
         }
         updateCamera = false;
@@ -512,8 +568,7 @@ function animate() {
                 camera.top = WIDTH / aspectRatio / 2;
                 camera.bottom = WIDTH / aspectRatio / -2;
             }
-        }
-        else if(window.innerHeight > 0 && window.innerWidth > 0 && camera.isPerspectiveCamera){
+        } else if (window.innerHeight > 0 && window.innerWidth > 0 && camera.isPerspectiveCamera) {
             camera.aspect = aspectRatio;
         }
 
@@ -522,31 +577,94 @@ function animate() {
     }
 
     /*Orange speed multiplier*/
-    var time=clock.getElapsedTime();
-    if((time/60)>1){
-        currentMultiplier=Math.min(currentMultiplier*ORANGE_SPEED_MULTIPLIER,MAX_MULTIPLIER);
+    var time = clock.getElapsedTime();
+    if ((time / 60) > 1) {
+        currentMultiplier = Math.min(currentMultiplier * ORANGE_SPEED_MULTIPLIER, MAX_MULTIPLIER);
         clock.start();
     }
 
-    time=orangeClock.getElapsedTime();
+    time = orangeClock.getElapsedTime();
     /* Orange Movement*/
-    if(time/(Math.floor(Math.random()*10)+5)>1 && oranges.length<3){
-        var dir =new THREE.Vector3(Math.random()*2-1,0,Math.random()*2-1).normalize()
-        var orange =createOrangeObstacle(Math.random()*WIDTH-WIDTH/2,Math.random()*HEIGHT-HEIGHT/2,dir.normalize(),Math.random()*4*currentMultiplier);
+    if (time / (Math.floor(Math.random() * 5) + 5) > 1 && oranges.length < NUMBER_ORANGES) {
+        var dir = new THREE.Vector3(Math.random() * 2 - 1, 0, Math.random() * 2 - 1).normalize()
+        var orange = createOrangeObstacle(Math.random() * WIDTH - WIDTH / 2, Math.random() * HEIGHT - HEIGHT / 2, dir.normalize(), Math.random() * 4 * currentMultiplier);
         scene.add(orange);
         oranges.push(orange);
         orangeClock.start();
     }
     var i;
-    for (i=0;i<oranges.length;i++){
+    for (i = 0; i < oranges.length; i++) {
         var d = new THREE.Vector3;
         d.copy(oranges[i].userData.direction);
         oranges[i].position.add(d.multiplyScalar(oranges[i].userData.speed));
-        d.cross(new THREE.Vector3(0,1,0))
-        var quaternion = new THREE.Quaternion().setFromAxisAngle(d,-oranges[i].userData.speed/20).normalize()
-        oranges[i].applyQuaternion(quaternion)
+        d.cross(new THREE.Vector3(0, 1, 0))
+        var quaternion = new THREE.Quaternion().setFromAxisAngle(d, -oranges[i].userData.speed / 20).normalize();
+        oranges[i].applyQuaternion(quaternion);
+        oranges[i].updateboundings();
+        if (oranges[i].position.x > WIDTH / 2 || oranges[i].position.x < WIDTH / -2 || oranges[i].position.z > HEIGHT / 2 || oranges[i].position.z < HEIGHT / -2) {
+            scene.remove(oranges[i]);
+            oranges.splice(i, 1);
+            i--;
+        }
     }
+
+    /* test for colisions */
+    scene.traverse(function (node) {
+        if (node instanceof colidable) {
+            scene.traverse(function (other) {
+                if (other instanceof colidable && node != other && intercept(node, other)) {
+                    handleColision(node, other);
+                }
+            });
+        }
+    });
 
     render();
     requestAnimationFrame(animate);
+}
+
+class colidable extends THREE.Object3D {
+    constructor() {
+        super();
+        this.boundingSphere = new THREE.Sphere;
+        this.boundingBox = new THREE.Box3;
+        this.iscolidable = true;
+    }
+
+    updateboundings() {
+        this.boundingBox.setFromObject(this);
+        this.boundingBox.getBoundingSphere(this.boundingSphere);
+    }
+}
+
+function handleColision(main, other) {
+    switch (main.userData.type) {
+        case "cheerio":
+            if (other.userData.type == "car" || other.userData.type == "cheerio") {
+                main.userData.speed = Math.max(other.userData.speed,main.userData.speed);
+
+                main.userData.direction.add(other.userData.direction).normalize();
+                var dist = main.boundingSphere.center.distanceTo(other.boundingSphere.center);
+                var direc = new THREE.Vector3(main.position.x-other.position.x, 0,  main.position.z-other.position.z).normalize()
+                main.position.add(direc.multiplyScalar(main.boundingSphere.radius + other.boundingSphere.radius - dist+1));
+                main.updateboundings();
+            }
+            break;
+        case "butter":
+            if (other.userData.type == "car"|| other.userData.type == "cheerio") {
+                other.userData.speed = 0;
+                var dist = main.boundingSphere.center.distanceTo(other.boundingSphere.center);
+                var direc = new THREE.Vector3(other.position.x - main.position.x, 0, other.position.z - main.position.z).normalize()
+                other.position.add(direc.multiplyScalar(main.boundingSphere.radius + other.boundingSphere.radius - dist));
+                other.updateboundings();
+            }
+            break;
+        case "orange":
+            if (other.userData.type == "car") {
+                scene.remove(other);
+                createCar();
+                updateCamera = true;
+            }
+            break;
+    }
 }
